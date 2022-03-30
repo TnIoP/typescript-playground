6aa209b718aadd9046a42b4f755d70bf2d55cde7
TnIoP <pointblank94@naver.com>
2022-03-30 11:25:32 +0900

    - study typescript basic

---
+++ typescript_prac/learn-typescript-master/class-note/1_type-basic.ts
@@ -0,0 +1,30 @@
+// js 문자열 선언
+// var str = 'hello'
+
+// ts 문자열 선언
+let str: string = 'hello';
+
+// ts 숫자
+let num: number = 10;
+
+// ts 배열
+let arr: Array<number> = [1, 2, 3]
+let alpha: Array<string> = ['a', 'b', 'c']
+let arr2: number[] = [4, 5, 6]
+
+// ts 튜플
+let temp: [string, number] = ['gangnam', 100];
+
+// ts 객체
+let obj: object = {}
+// let person: object = {
+//     name: 'capt',
+//     age: 100
+// };
+let person: {name: string, age: number} = {
+    name: 'thor',
+    age: 1000
+}
+
+// ts 진위값
+let show: boolean = true
---
+++ typescript_prac/learn-typescript-master/why-ts/sample.js
@@ -0,0 +1,18 @@
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// sum(10, '20') // 1020
+
+// @ts-check
+
+/**
+ * @param {number} a
+ * @param {number} b
+ */
+
+function sum(a, b) {
+    return a+b
+}
+
+sum(10, 20);
